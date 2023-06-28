c6f501294985c2f10161b40b51b62523459d1196
Leroy <leroy.davidson@live.nl>
2023-06-28 22:00:35 +0200

    Create boilerplate and wildshape API

---
+++ app/api/wildshape/route.js
@@ -0,0 +1,31 @@
+import { NextResponse } from 'next/server'
+import wildshapes from './wildshapes.json'
+import Fuse from 'fuse.js'
+
+export async function GET(request) {
+    const options = {
+        includeScore: true,
+        keys: [
+            {
+            name: 'index',
+            weight: 0.3
+            },
+            {
+            name: 'name',
+            weight: 1
+            },
+            {
+            name: 'url',
+            weight: 0.2
+            }
+        ],
+        limit: 2,
+        threshold: 0.5
+        }
+
+    const fuse = new Fuse(wildshapes, options)
+    const { searchParams } = new URL(request.url)
+    const results = fuse.search(searchParams.get('q'))
+
+    return NextResponse.json(results)
+}
