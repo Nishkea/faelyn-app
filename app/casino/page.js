7cca9a4b004b95949ae7ac06d7d1f6c9141d7898
Nishkea <leroy.davidson@live.nl>
2023-06-30 15:45:46 +0200

    Add background to casino page

--- app/casino/page.js
+++ app/casino/page.js
@@ -1,7 +1,23 @@
+'use client'
+
+import { useRouter } from "next/navigation"
+
 export default function Casino() {
-    return (
-        <div className='w-screen h-screen flex flex-wrap gap-4 p-5'>
-            Casino
-        </div>
-    )
+  const router = useRouter()
+
+  return (
+    <div className='relative w-screen h-screen flex justify-center items-center gap-4 p-5'>
+      <div onClick={() => router.push('/')}  className='hover:scale-[1.05] duration-200 absolute top-5 left-5 cursor-pointer z-20'>
+        <div className='bg-black text-white p-4 rounded-xl'>Back to menu</div>
+      </div>
+
+      <main className='z-20 relative'>
+        <p>Casino</p>
+      </main>
+
+      <div className='absolute top-0 object-cover blur-[5px] left-0 w-full h-full z-10'>
+        <img className='w-full h-full object-cover' src='https://directus.shoto.studio/assets/9dff7b68-073a-4ed1-9bd5-9bc3f0ea217d.jpg' />
+      </div>
+    </div>
+  )
 }
